1fbe60568d4fb2dc05a51538f1497ed1dd176b57
Bagus Ekaputra <bagusekaputra004@gmail.com>
2025-09-17 11:26:14 +0700

    ubah kode css

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  base: '/public/build/', // <- sesuaikan path hosting
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
 });
