9d222e9e0e5861bbfcb32d88dabfab9dca744187
Bagus Ekaputra <bagusekaputra004@gmail.com>
2025-09-17 11:15:07 +0700

    ubah kode css di welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,11 @@
+@extends('layout.app')
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perpustakaan Mini</title>
-   @extends('layout.app')
 </head>
 <body class="bg-gray-100 text-gray-800 font-sans">
 
