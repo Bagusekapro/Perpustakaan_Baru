72bb9c30ddfb41334a684e9a2f1a15a6a8660d45
Bagus Ekaputra <bagusekaputra004@gmail.com>
2025-09-17 11:10:37 +0700

    ubah kode css di welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perpustakaan Mini</title>
+   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body class="bg-gray-100 text-gray-800 font-sans">
 
